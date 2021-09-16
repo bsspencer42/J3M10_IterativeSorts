c44bffe37c0f03c3d146c586d3a51dc5e20d9c89
bsspencer42 <bsspencer42@gmail.com>
2021-09-16 10:51:32 -0400

    Initial commit

---
+++ src/Sorting.java
@@ -0,0 +1,149 @@
+import java.util.Comparator;
+
+/**
+ * Your implementation of various iterative sorting algorithms.
+ */
+public class Sorting {
+
+    /**
+     * Implement bubble sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * NOTE: You should implement bubble sort with the last swap optimization.
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+       int beginIndex = 0;
+       int currIndex = 0;
+       int endIndex = arr.length-1;
+        T temp;
+       // Helper Vars
+        int numComps = 0;
+        int numSwaps = 0;
+        int lastSwap;
+
+        while (endIndex != 0){
+            // Reset the lastSwap value to the default of the endIndex
+            // Reset currIndex pointer
+            lastSwap = endIndex;
+            currIndex = 0;
+
+            // Debug Printing
+            System.out.println("Loop " + (arr.length-endIndex));
+            //
+
+            // Inner Loop
+            while (currIndex < endIndex){
+                // Increment comp counter
+                numComps++;
+                // Evaluate for swap
+                if (comparator.compare(arr[currIndex],arr[currIndex+1]) > 0){
+                    // Perform Swap
+                    temp = arr[currIndex+1];
+                    arr[currIndex+1] = arr[currIndex];
+                    arr[currIndex] = temp;
+                    numSwaps++;
+                    // Save index of last swap
+                    lastSwap = currIndex;
+                }
+                // Debug Print - Print after each swap
+                printArr(arr);
+                //
+
+                // Increment index
+                currIndex++;
+            } // End Inner Loop
+
+            // Last Swapped Optimization
+            if (lastSwap != endIndex){
+                endIndex = lastSwap;
+            }
+            else {
+                endIndex--;
+            }
+        }
+        System.out.println("Total comparisons: " + numComps);
+        System.out.println("Total swaps: " + numSwaps);
+    }
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be:
+     * in-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n^2)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    public static <T> void printArr(T[] arr){
+        String myString = "[";
+            for (int i = 0; i < arr.length;i++){
+                if (i != arr.length && i != 0)
+                    myString += ", ";
+                myString += arr[i];
+            }
+        myString+="]";
+        System.out.println(myString);
+    }
+
+    public static void main(String[] args) {
+        Integer[] myArr = {4,3,1,5,2,6,7};
+        Comparator<Integer> myComp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        };
+        bubbleSort(myArr,myComp);
+    }
+
+}
